a734879f3e5da6aa6d0e2c464f17013fc844af75
david <davmwa90@gmail.com>
2022-11-30 09:16:51 +0300

    Declared different functions and variables

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+// declaring a global variable using var; 
+var customerName = "bob"; 
+
+// function that has access to customer and upperCaseIt; 
+function upperCaseCustomerName() {
+    window.customerName = window.customerName.toUpperCase(); 
+}
+// function that declares a function at a global scope; 
+
+function setBestCustomer() {
+    window.bestCustomer = "not bob"
+}
+// function that overwrites the previously declared global variable; 
+function overwriteBestCustomer() {
+    window.bestCustomer = "maybe bob"; 
+}
+// Declaring a const global variable and trying to overwrite it from within a function; 
+const leastFavoriteCustomer = "James"; 
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Martin"; 
+}
